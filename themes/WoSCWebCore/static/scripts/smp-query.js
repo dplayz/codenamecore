b354343e020422f7371d0ce98a239d8ff501cdc6
dplayz <dp@dplayzgames06.tk>
2022-01-18 19:28:44 +0800

    scripts

---
+++ themes/WoSCWebCore/static/scripts/smp-query.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function( $ ){
+	$.getJSON('https://api.mcsrvstat.us/bedrock/2/smp.worldofsteelcraft.tk:33158', function(data) {      
+		$('#hostname').text(data.hostname);
+		$('#port').text(data.port);
+		$('#version').text(data.version);
+		$('#mapname').text(data.map);
+		$('#isonline').text(data.online);
+		$('#playeronline').text(data.players.online);
+		$('#playermax').text(data.players.max);
+		$('#motd').text(data.motd.html[0]);
+	});
+});
